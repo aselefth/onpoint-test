2145584fec2107b760a5c8e6fc5dd9454d95cbd1
aselefth <andrei200287@gmail.com>
2022-10-16 02:40:52 +0300

    third page animations

--- src/pages/ThirdPage.tsx
+++ src/pages/ThirdPage.tsx
@@ -5,12 +5,73 @@ import bubble3 from "../assets/images/bubble3.png";
 import bubble4 from "../assets/images/bubble4.png";
 import leftLogo from '../assets/images/icon2.png';
 import rightLogo from '../assets/images/icon1.png';
+import { useState } from "react";
 
 
 const ThirdPage: React.FC = () => {
+  const [page, setPage] = useState(1);
+  const [isModal, setIsModal] = useState(false);
   return (
     <div className="container">
       <div className={styles.hero}>
+        {isModal && 
+        <div 
+          onClick={() => setIsModal(false)}
+          className={styles.modal__wrapper}
+        >
+          <div 
+            onClick={(e) => e.stopPropagation()}
+            className={styles.modal}
+          >
+            <div 
+              onClick={() =>setIsModal(false)}
+              className={styles.close_modal}
+            >
+            </div>
+            {page == 1 && <div className={styles.modal__page}>
+              <div className={styles.modal__page__element}>
+                <span>01</span>
+                <p>lorem ipsum dolor sit amet consectetur adipiscing elit</p>
+              </div>
+              <div className={styles.modal__page__element}>
+                <span>02</span>
+                <p>Faucibus pulnivefwe elementum interger enim</p>
+              </div>
+              <div className={styles.modal__page__element}>
+                <span>03</span>
+                <p>lorem ipsum dolor sit amet consectetur adipiscing elit</p>
+              </div>
+            </div>}
+            {page == 2 && <div className={styles.modal__page}>
+              <div className={styles.modal__page__element}>
+                <span>04</span>
+                <p>lorem ipsum dolor sit amet consectetur adipiscing elit</p>
+              </div>
+              <div className={styles.modal__page__element}>
+                <span>05</span>
+                <p>Faucibus pulnivefwe elementum interger enim</p>
+              </div>
+              <div className={styles.modal__page__element}>
+                <span>06</span>
+                <p>lorem ipsum dolor sit amet consectetur adipiscing elit</p>
+              </div>
+            </div>}
+            <div className={styles.pagination}>
+              <span
+                onClick={() => setPage(1)}
+              >
+                {'<'}
+              </span>
+              <p className={page == 1 ? styles.active : styles.nonactive}></p>
+              <p className={page == 2 ? styles.active : styles.nonactive}></p>
+              <span
+                onClick={() => setPage(2)}
+              >
+                {'>'}
+              </span>
+            </div>
+          </div>
+        </div>}
         <img src={bottle} className={styles.bottle} />
         <div className={styles.bubble__container}>
           <img src={bubble2} className={styles.bubble} />
@@ -28,7 +89,8 @@ const ThirdPage: React.FC = () => {
           <img src={bubble3} className={styles.bubble} />
         </div>
         <div className={styles.hero__header}>
-          <p>ключевое сообщение</p>
+          {!isModal && <p>ключевое сообщение</p>}
+          {isModal && <p>преимущества</p>}
           <h1>
             brend<span>xy</span>
           </h1>
@@ -50,7 +112,10 @@ const ThirdPage: React.FC = () => {
             <div className={styles.content__right__top}>
               A arcu cursus vitae
             </div>
-            <div className={styles.button}>
+            <div 
+              onClick={() => setIsModal(true)}
+              className={styles.button}
+            >
               <div className={styles.arrow}>
                 <div className={styles.arrow__black}>+</div>
               </div>
